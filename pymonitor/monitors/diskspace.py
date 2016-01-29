8ee8d871bf36e05659dfde8e0b606d6b5cd5a8dd
Dave Pedu <git@davepedu.com>
2016-01-28 22:31:57 -0800

    add inode count to diskspace check

--- pymonitor/monitors/diskspace.py
+++ pymonitor/monitors/diskspace.py
@@ -9,11 +9,17 @@ def diskspace(filesystems=[]):
             "fs.raw": fs,
             "diskfree": stats.f_bsize * stats.f_bavail,
             "diskused": (stats.f_blocks-stats.f_bavail) * stats.f_bsize,
-            "disksize": stats.f_bsize * stats.f_blocks
+            "disksize": stats.f_bsize * stats.f_blocks,
+            "inodesmax": stats.f_files,
+            "inodesfree": stats.f_favail,
+            "inodesused": stats.f_files - stats.f_favail
         }
         
-        info["diskpctused"] = round(info["diskused"]/info["disksize"], 2)
-        info["diskpctfree"] = round(info["diskfree"]/info["disksize"], 2)
+        info["diskpctused"] = round(info["diskused"]/info["disksize"] if info["disksize"] > 0 else 0, 2)
+        info["diskpctfree"] = round(info["diskfree"]/info["disksize"] if info["disksize"] > 0 else 0, 2)
+        
+        info["inodesused_pct"] = round(info["inodesused"] / info["inodesmax"] if info["inodesmax"] > 0 else 0, 2)
+        info["inodesfree_pct"] = round(info["inodesfree"] / info["inodesmax"] if info["inodesmax"] > 0 else 0, 2)
         
         yield info
 
